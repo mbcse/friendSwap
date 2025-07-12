5ef55df02c91623b4608ebbee9d29cfa9cd1c4b5
mbcse <mbcse50@gmail.com>
2025-07-12 10:30:00 +0530

    Add initial escrow interface definitions for contract interaction

---
+++ friendswap/src/interfaces/IEscrowSrc.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import { IEscrow } from "./IEscrow.sol";
+import { IBaseEscrow } from "./IBaseEscrow.sol";
+
+interface IEscrowSrc is IEscrow {
+    function publicWithdraw(bytes32 secret, IBaseEscrow.ExecutionData calldata executionData) external;   
+
+    function publicCancel(IBaseEscrow.ExecutionData calldata executionData) external;
+}
